8116fc2686461f8c14f89a6856d8b223d76b8a35
aquamark-io <heyaquamark@gmail.com>
2025-05-13 20:25:12 -0700

    Update background.js

--- background.js
+++ background.js
@@ -40,7 +40,7 @@ async function fetchLatestLogoUrl(userEmail) {
   const response = await fetch(listUrl, {
     headers: {
       apikey: 'YOUR_SUPABASE_ANON_KEY',
-      Authorization: 'Bearer YOUR_SUPABASE_ANON_KEY'
+      Authorization: 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImR2em1uaWtydmt2Z3JhZ3pocm9mIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDM5Njg5NzUsImV4cCI6MjA1OTU0NDk3NX0.FaHsjIRNlgf6YWbe5foz0kJFtCO4FuVFo7KVcfhKPEk'
     }
   });
   const data = await response.json();
